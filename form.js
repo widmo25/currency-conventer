b0d1c39f1c5e8e0bd1a9ad7a188516a39945804d
Michał Wodniczak <michal.wodniczak@o2.pl>
2021-05-02 14:23:33 +0200

    added function

--- form.js
+++ form.js
@@ -1,30 +1,42 @@
-console.log("Hello World!!!");
+{
+    const welcome = () => {
+        console.log("Hello World!!!");
+    }
 
-let formElement = document.querySelector(".js-form");
-let inputElement = document.querySelector(".js-input");
-let resultElement = document.querySelector(".js-result");
+    const calculateResult = (checkedCurrency) => {
+        const inputElement = document.querySelector(".js-input");
+        const resultElement = document.querySelector(".js-result");
+        const euroRate = 4.55;
+        const usdRate = 3.78;
+        const poundRate = 5.27;
+        switch (checkedCurrency.value) {
+            case "eur":
+                let euroChange = inputElement.value / euroRate;
+                resultElement.innerHTML = `za ${inputElement.value}zł otrzymamy ${euroChange.toFixed(2)} Euro`;
+                break;
 
-let euroRate = 4.55;
-let usdRate = 3.78;
-let poundRate = 5.27;
+            case "usd":
+                let dollarChange = inputElement.value / usdRate;
+                resultElement.innerHTML = `za ${inputElement.value}zł otrzymamy ${dollarChange.toFixed(2)} Dolarów`;
+                break;
 
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-    let checkedCurrency = document.querySelector(".js-currency:checked");
-    switch (checkedCurrency.value) {
-        case "eur":
-            let euroChange = inputElement.value / euroRate;
-            resultElement.innerHTML = `za ${inputElement.value}zł otrzymamy ${euroChange.toFixed(2)} Euro`;
-            break;
+            case "pound":
+                let poundChange = inputElement.value / poundRate;
+                resultElement.innerHTML = `za ${inputElement.value}zł otrzymamy ${poundChange.toFixed(2)} Funtów`;
+                break;
+        };
+    }
 
-        case "usd":
-            let dollarChange = inputElement.value / usdRate;
-            resultElement.innerHTML = `za ${inputElement.value}zł otrzymamy ${dollarChange.toFixed(2)} Dolarów`;
-            break;
-
-        case "pound":
-            let poundChange = inputElement.value / poundRate;
-            resultElement.innerHTML = `za ${inputElement.value}zł otrzymamy ${poundChange.toFixed(2)} Funtów`;
-            break;
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        let checkedCurrency = document.querySelector(".js-currency:checked");
+        calculateResult(checkedCurrency);
     };
-});
+
+    const init = () => {
+        welcome();
+        const formElement = document.querySelector(".js-form");
+        formElement.addEventListener("submit", onFormSubmit);
+    }
+    init();
+}

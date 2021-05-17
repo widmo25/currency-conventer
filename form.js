5666605a1a3b0d767acde54550e1d843c933094d
Michał Wodniczak <michal.wodniczak@o2.pl>
2021-05-17 18:25:41 +0200

    added onChangeResultText

--- form.js
+++ form.js
@@ -3,35 +3,36 @@
         console.log("Hello World!!!");
     }
 
-    const calculateResult = (checkedCurrency) => {
-        const inputElement = document.querySelector(".js-input");
-        const resultElement = document.querySelector(".js-result");
-        let result;
+    const calculateResult = (checkedCurrency, currency) => {
         const euroRate = 4.55;
         const usdRate = 3.78;
         const poundRate = 5.27;
         switch (checkedCurrency.value) {
             case "eur":
-                result = inputElement.value / euroRate;
-                resultElement.innerHTML = `za ${inputElement.value}zł otrzymamy ${result.toFixed(2)} Euro`;
-                break;
+                return currency / euroRate;
 
             case "usd":
-                result = inputElement.value / usdRate;
-                resultElement.innerHTML = `za ${inputElement.value}zł otrzymamy ${result.toFixed(2)} Dolarów`;
-                break;
+                return currency / usdRate;
 
             case "pound":
-                result = inputElement.value / poundRate;
-                resultElement.innerHTML = `za ${inputElement.value}zł otrzymamy ${result.toFixed(2)} Funtów`;
-                break;
+                return currency / poundRate;
         };
     }
+
+    const onChangeResultText = (checkedCurrency, currency) => {
+        const resultElement = document.querySelector(".js-result");
+        const result = calculateResult(checkedCurrency,currency)
+        resultElement.innerHTML = `Za ${currency}zł otrzymamy ${result.toFixed(2)} ${checkedCurrency.value}`
+    }
+
 
     const onFormSubmit = (event) => {
         event.preventDefault();
         let checkedCurrency = document.querySelector(".js-currency:checked");
-        calculateResult(checkedCurrency);
+        const inputElement = document.querySelector(".js-input");
+        const currency = +inputElement.value;
+        calculateResult(checkedCurrency, currency);
+        onChangeResultText(checkedCurrency, currency)
     };
 
     const init = () => {
